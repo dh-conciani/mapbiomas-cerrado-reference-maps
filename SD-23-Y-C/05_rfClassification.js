5161cc54f48e24f1389394b52d3d8052952755e1
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-10-16 16:41:48 -0300

    Update 05_rfClassification.js

--- SD-23-Y-C/05_rfClassification.js
+++ SD-23-Y-C/05_rfClassification.js
@@ -17,4 +17,57 @@ var carta = ee.FeatureCollection('projects/nexgenmap/ANCILLARY/nextgenmap_grids'
   // compute 300m buffer
   .map(function(feature) {
     return feature.buffer({'distance': 300});
-  });
+});
+
+// build a raster for the carta
+var carta_img = ee.Image(1).clip(carta);
+
+// read sentinel-2 mosaic
+var mosaic = ee.ImageCollection('projects/nexgenmap/MapBiomas2/SENTINEL/mosaics-3')
+  .filter(ee.Filter.eq('version', '3'))
+  .filter(ee.Filter.eq('biome', 'CERRADO'))
+  .filterBounds(carta);
+
+// set reference year
+var year = 2020;
+
+// read training samples
+var trainingSamples = ee.FeatureCollection('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_training_v1');
+
+// compute aditional bands for thye sentinel-2 mosaic
+var geo_coordinates = ee.Image.pixelLonLat().clip(carta);
+// get latitude
+var lat = geo_coordinates.select('latitude').add(5).multiply(-1).multiply(1000).toInt16();
+// get longitude
+var lon_sin = geo_coordinates.select('longitude').multiply(Math.PI).divide(180)
+  .sin().multiply(-1).multiply(10000).toInt16().rename('longitude_sin');
+// longitude cosine
+var lon_cos = geo_coordinates.select('longitude').multiply(Math.PI).divide(180)
+  .cos().multiply(-1).multiply(10000).toInt16().rename('longitude_cos');
+
+// get heigth above nearest drainage
+var hand = ee.ImageCollection('users/gena/global-hand/hand-100').mosaic().toInt16()
+  .clip(carta).rename('hand');
+  
+// get the sentinel mosaic for the current year 
+var mosaic_i = mosaic.filterMetadata('year', 'equals', 2020)
+  .mosaic()
+  .updateMask(carta_img)
+  // add auxiliary bands
+  .addBands(lat)
+  .addBands(lon_sin)
+  .addBands(lon_cos)
+  .addBands(hand);
+  
+print(mosaic_i)
+
+// train classifier
+var classifier = ee.Classifier.smileRandomForest({
+  'numberOfTrees': 300,
+  'variablesPerSplit': 20
+  }).train(trainingSamples, 'reference', mosaic_i.bandNames());
+
+// perform classificationn 
+var predicted = mosaic_i.classify(classifier).mask(mosaic_i.select(0)).rename('classification_' + year).toInt8();
+
+Map.addLayer(predicted)
