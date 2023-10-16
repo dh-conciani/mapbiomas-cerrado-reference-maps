67078501f814350b2b3722c463a7a8c14387dc77
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-10-16 16:20:03 -0300

    Update 04_getSignatures.js

--- SD-23-Y-C/04_getSignatures.js
+++ SD-23-Y-C/04_getSignatures.js
@@ -23,21 +23,44 @@ var mosaic = ee.ImageCollection('projects/nexgenmap/MapBiomas2/SENTINEL/mosaics-
   .filter(ee.Filter.eq('version', '3'))
   .filter(ee.Filter.eq('biome', 'CERRADO'))
   .filterBounds(carta);
-  
-  Map.addLayer(mosaic)
-  
 
+// set reference year
+var year = 2020;
 
 // import sample points
-samples = ee.FeatureCollection('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_samplePoints_v1');
+var samples = ee.FeatureCollection('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_samplePoints_v1');
 
 // compute aditional bands for thye sentinel-2 mosaic
-//var geo_coordinates =ee.Image.pixelLonLat().clip(carta)
-//    ## get latitude
-//    lat <- geo_coordinates$select('latitude')$add(5)$multiply(-1)$multiply(1000)$toInt16()
-//    ## get longitude
-//    lon_sin <- geo_coordinates$select('longitude')$multiply(pi)$divide(180)$
-//      sin()$multiply(-1)$multiply(10000)$toInt16()$rename('longitude_sin')
-//    ## cosine
-//    lon_cos <- geo_coordinates$select('longitude')$multiply(pi)$divide(180)$
-//      cos()$multiply(-1)$multiply(10000)$toInt16()$rename('longitude_cos')
+var geo_coordinates = ee.Image.pixelLonLat().clip(carta);
+// get latitude
+var lat = geo_coordinates.select('latitude').add(5).multiply(-1).multiply(1000).toInt16();
+// get longitude
+var lon_sin = geo_coordinates.select('longitude').multiply(Math.PI).divide(180)
+  .sin().multiply(-1).multiply(10000).toInt16().rename('longitude_sin');
+// longitude cosine
+var lon_cos = geo_coordinates.select('longitude').multiply(Math.PI).divide(180)
+  .cos().multiply(-1).multiply(10000).toInt16().rename('longitude_cos');
+
+// get heigth above nearest drainage
+var hand = ee.ImageCollection('users/gena/global-hand/hand-100').mosaic().toInt16()
+  .clip(carta).rename('hand');
+  
+// get the sentinel mosaic for the classification year
+var mosaic_i = mosaic.filterMetadata('year', 'equals', year).mosaic();
+
+// plot sentinel mosaic
+Map.addLayer(mosaic, {'bands': ['swir1_median', 'nir_median', 'red_median'],
+  'gain': [0.08, 0.07, 0.2], 'gamma': 0.85}, 'Sentinel ' + year, true);
+  
+// build mosaic with complementary bands
+mosaic_i = mosaic_i.addBands(lat).addBands(lon_sin).addBands(lon_cos);
+
+// get training samples
+var training_i = mosaic_i.sampleRegions({'collection': samples,
+                                         'scale': 10,
+                                         'geometries': true,
+                                         'tileScale': 2});
+// export as GEE asset
+Export.table.toAsset({'collection': training_i,
+                      'description': id_carta + '_training_v' + version_output,
+                      'assetId':  output_dir + '/' + id_carta + '_training_v' + version_output});
