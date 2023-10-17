9504ce06784f751b738399734ed3f4aaca2f25d2
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-10-17 12:57:00 -0300

    Update 07_applySegments.js

--- SD-23-Y-C/07_applySegments.js
+++ SD-23-Y-C/07_applySegments.js
@@ -4,8 +4,14 @@
 // set ibges id
 var id_carta = 'SD-23-Y-C';
 
+// define output version
+var output_version = 1; 
+
+// output directory
+var output_dir = 'users/dh-conciani/gt_mapa_referencia/' + id_carta;
+
 // read collection 
-var collection = ee.Image('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_classification_v1');
+var classification = ee.Image('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_classification_v1');
 
 // read segments
 var segments = ee.Image('users/dh-conciani/gt_mapa_referencia/SD-23-Y-CSD-23-Y-C_segments_v1');
@@ -19,7 +25,7 @@ var carta = ee.FeatureCollection('projects/nexgenmap/ANCILLARY/nextgenmap_grids'
 });
 
 // compute the mode class 
-var mode = segments.addBands(collection)
+var mode = segments.addBands(classification)
                       .reduceConnectedComponents({
                         'reducer': ee.Reducer.mode(), 
                         'labelBand': 'segments'
@@ -35,6 +41,17 @@ var vis = {
     'palette': require('users/mapbiomas/modules:Palettes.js').get('classification8')
 };
 
+Map.addLayer(classification, vis, 'classification');
 Map.addLayer(mode, vis, 'classification + segmentation');
 
+// Export segments
+Export.image.toAsset({
+	image: mode,
+  description: id_carta + '_segmentMode_v' + output_version,
+  assetId: output_dir + id_carta + '_segmentsMode_v' + output_version,
+  pyramidingPolicy: 'mode',
+  region: classification.geometry(),
+  scale: 10,
+  maxPixels: 1e13,
+});
 
