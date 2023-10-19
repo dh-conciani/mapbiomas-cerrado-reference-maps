3506afbeaa5f8341df627941cb88b3ca1cfb2c41
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-10-19 09:57:09 -0300

    Update 09_exportData.js

--- SD-23-Y-C/09_exportData.js
+++ SD-23-Y-C/09_exportData.js
@@ -1,13 +1,16 @@
 // export data to perform manual inspection 
+// dhemerson.costa@ipam.org.br
 
-// read mode
-var mode = ee.Image('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_segmentsMode_v1');
+// read input data 
 
-// read raw classification
+// raw classification
 var raw = ee.Image('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_classification_v1');
 
-// read segments
+// segments
 var segments = ee.Image('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_segments_v1');
+
+// classification mode by segment
+var mode = ee.Image('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/SD-23-Y-C_segmentsMode_v1');
 
 // read terra class
 var terra_class = ee.Image('projects/barbaracosta-ipam/assets/base/TERRACLASS_cerrado_2020')
@@ -15,10 +18,23 @@ var terra_class = ee.Image('projects/barbaracosta-ipam/assets/base/TERRACLASS_ce
   .remap([1, 2, 9, 10, 12, 13, 14, 15, 16, 17, 19, 20, 22, 51, 52],
          [3, 3, 9, 21, 21, 21, 21, 21, 30, 24, 21, 33, 27, 21, 24]);
 
-// mapa do df
+// mapa do DF
 var df_ref = ee.Image('projects/barbaracosta-ipam/assets/base/DF_cobertura-do-solo_2019_img')
   .select('classification_DF_2019');
 
+// sentinel mosaic
+var mosaic = ee.ImageCollection('projects/nexgenmap/MapBiomas2/SENTINEL/mosaics-3')
+  .filter(ee.Filter.eq('version', '3'))
+  .filter(ee.Filter.eq('biome', 'CERRADO'))
+  .filter(ee.Filter.eq('year', 2020))
+  .mosaic()
+  .updateMask(mode);
+
+// get planet
+
+
+
+// export 
 Export.image.toDrive({
 		image:mode,
     description: 'SD-23-Y-C_segmentsMode_v1',
@@ -74,3 +90,35 @@ Export.image.toDrive({
     fileFormat: 'GeoTIFF',
 });
 
+Export.image.toDrive({
+		image: mosaic.select(['swir1_median', 'nir_median', 'red_median']),
+    description: 'SD-23-Y-C_sentinelMedian_v1',
+    folder: 'MAPA_REFERENCIA',
+    region: raw.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: mosaic.select(['swir1_median_wet', 'nir_median_wet', 'red_median_wet']),
+    description: 'SD-23-Y-C_sentinelMedianWet_v1',
+    folder: 'MAPA_REFERENCIA',
+    region: raw.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: mosaic.select(['swir1_median_dry', 'nir_median_dry', 'red_median_dry']),
+    description: 'SD-23-Y-C_sentinelMedianDry_v1',
+    folder: 'MAPA_REFERENCIA',
+    region: raw.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
