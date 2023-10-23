fedf7e003a7e7434ca1e79a267cdb0ef9ca78e80
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-10-23 15:45:46 -0300

    Update 09_exportData.js

--- SD-23-Y-C/09_exportData.js
+++ SD-23-Y-C/09_exportData.js
@@ -1,6 +1,12 @@
 // export data to perform manual inspection 
 // dhemerson.costa@ipam.org.br
 
+// read tile
+var tile = ee.FeatureCollection('users/dh-conciani/gt_mapa_referencia/SD-23-Y-C/selected_subTile');
+
+// set output
+var output = 'mapa-ref-tile';
+
 // read input data 
 
 // raw classification
@@ -30,6 +36,21 @@ var mosaic = ee.ImageCollection('projects/nexgenmap/MapBiomas2/SENTINEL/mosaics-
   .mosaic()
   .updateMask(mode);
 
+// sentinel mosaic
+var mosaic_prev = ee.ImageCollection('projects/nexgenmap/MapBiomas2/SENTINEL/mosaics-3')
+  .filter(ee.Filter.eq('version', '3'))
+  .filter(ee.Filter.eq('biome', 'CERRADO'))
+  .filter(ee.Filter.eq('year', 2019))
+  .mosaic()
+  .updateMask(mode);
+
+var mosaic_post = ee.ImageCollection('projects/nexgenmap/MapBiomas2/SENTINEL/mosaics-3')
+  .filter(ee.Filter.eq('version', '3'))
+  .filter(ee.Filter.eq('biome', 'CERRADO'))
+  .filter(ee.Filter.eq('year', 2021))
+  .mosaic()
+  .updateMask(mode);
+
 // planet
 var analytic_202009 = ee.Image('projects/planet-nicfi/assets/basemaps/americas/planet_medres_normalized_analytic_2020-09_mosaic')
   .updateMask(mode);
@@ -38,14 +59,23 @@ var analytic_202012 = ee.Image('projects/planet-nicfi/assets/basemaps/americas/p
   .updateMask(mode);
 
 
-Map.addLayer(analytic_202009, {min:[200,200,50],max:[1700,1400,1100],gamma: 1.35, bands:["R", "G", "B"]}, 'PLANET 2020-09', false);
-Map.addLayer(analytic_202012, {min:[200,200,50],max:[1700,1400,1100],gamma: 1.35, bands:["R", "G", "B"]}, 'PLANET 2020-12', false);
+var analytic_202109 = ee.Image('projects/planet-nicfi/assets/basemaps/americas/planet_medres_normalized_analytic_2021-09_mosaic')
+  .updateMask(mode);
+  
+var analytic_202111 = ee.Image('projects/planet-nicfi/assets/basemaps/americas/planet_medres_normalized_analytic_2021-11_mosaic')
+  .updateMask(mode);
+
+
+
+
+Map.addLayer(analytic_202109, {min:[200,200,50],max:[1700,1400,1100],gamma: 1.35, bands:["R", "G", "B"]}, 'PLANET 2020-09', false);
+Map.addLayer(analytic_202111, {min:[200,200,50],max:[1700,1400,1100],gamma: 1.35, bands:["R", "G", "B"]}, 'PLANET 2020-12', false);
 
 Export.image.toDrive({
 		image: analytic_202009,
     description: 'SD-23-Y-C_planet2020-09_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
     scale: 4.77,
     maxPixels: 1e13,
     skipEmptyTiles: true,
@@ -55,13 +85,37 @@ Export.image.toDrive({
 Export.image.toDrive({
 		image: analytic_202012,
     description: 'SD-23-Y-C_planet2020-12_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
     scale: 4.77,
     maxPixels: 1e13,
     skipEmptyTiles: true,
     fileFormat: 'GeoTIFF',
 });
+
+
+Export.image.toDrive({
+		image: analytic_202109,
+    description: 'SD-23-Y-C_planet2021-09_v1',
+    folder: output,
+    region: tile.geometry(),
+    scale: 4.77,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: analytic_202111,
+    description: 'SD-23-Y-C_planet2021-11_v1',
+    folder: output,
+    region: tile.geometry(),
+    scale: 4.77,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
 
 // read study area
 var carta = ee.FeatureCollection('projects/nexgenmap/ANCILLARY/nextgenmap_grids')
@@ -86,7 +140,7 @@ var aps = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/areas-prot
 Export.table.toDrive({
 		collection: aps,
     description: 'SD-23-Y-C_protectedAreas',
-    folder: 'MAPA_REFERENCIA',
+    folder: output,
     fileFormat: 'SHP',
 });
 
@@ -94,14 +148,14 @@ Export.table.toDrive({
 Export.table.toDrive({
 		collection: carta,
     description: 'SD-23-Y-C_Carta',
-    folder: 'MAPA_REFERENCIA',
+    folder: output,
     fileFormat: 'SHP',
 });
 
 Export.table.toDrive({
 		collection: subcarta,
     description: 'SD-23-Y-C_subCarta',
-    folder: 'MAPA_REFERENCIA',
+    folder: output,
     fileFormat: 'SHP',
 });
 
@@ -109,8 +163,8 @@ Export.table.toDrive({
 Export.image.toDrive({
 		image:mode,
     description: 'SD-23-Y-C_segmentsMode_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: segments.geometry(),
+    folder: output,
+    region: tile.geometry(),
     scale: 10,
     maxPixels: 1e13,
     skipEmptyTiles: true,
@@ -120,8 +174,8 @@ Export.image.toDrive({
 Export.image.toDrive({
 		image: raw,
     description: 'SD-23-Y-C_classification_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
     scale: 10,
     maxPixels: 1e13,
     skipEmptyTiles: true,
@@ -131,8 +185,8 @@ Export.image.toDrive({
 Export.image.toDrive({
 		image: segments,
     description: 'SD-23-Y-C_segments_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
     scale: 10,
     maxPixels: 1e13,
     skipEmptyTiles: true,
@@ -142,8 +196,8 @@ Export.image.toDrive({
 Export.image.toDrive({
 		image: terra_class,
     description: 'SD-23-Y-C_terraClass_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
     scale: 10,
     maxPixels: 1e13,
     skipEmptyTiles: true,
@@ -153,8 +207,8 @@ Export.image.toDrive({
 Export.image.toDrive({
 		image: df_ref,
     description: 'SD-23-Y-C_refDistritoFederal_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
     scale: 10,
     maxPixels: 1e13,
     skipEmptyTiles: true,
@@ -164,8 +218,52 @@ Export.image.toDrive({
 Export.image.toDrive({
 		image: mosaic.select(['swir1_median', 'nir_median', 'red_median']),
     description: 'SD-23-Y-C_sentinelMedian_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: mosaic_prev.select(['swir1_median', 'nir_median', 'red_median']),
+    description: 'SD-23-Y-C_sentinelMedianPrev_v1',
+    folder: output,
+    region: tile.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: mosaic_post.select(['swir1_median', 'nir_median', 'red_median']),
+    description: 'SD-23-Y-C_sentinelMedianPost_v1',
+    folder: output,
+    region: tile.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: mosaic_prev.select(['swir1_median_wet', 'nir_median_wet', 'red_median_wet']),
+    description: 'SD-23-Y-C_sentinelMedianWetPrev_v1',
+    folder: output,
+    region: tile.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: mosaic_post.select(['swir1_median_wet', 'nir_median_wet', 'red_median_wet']),
+    description: 'SD-23-Y-C_sentinelMedianWetPost_v1',
+    folder: output,
+    region: tile.geometry(),
     scale: 10,
     maxPixels: 1e13,
     skipEmptyTiles: true,
@@ -175,8 +273,8 @@ Export.image.toDrive({
 Export.image.toDrive({
 		image: mosaic.select(['swir1_median_wet', 'nir_median_wet', 'red_median_wet']),
     description: 'SD-23-Y-C_sentinelMedianWet_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
     scale: 10,
     maxPixels: 1e13,
     skipEmptyTiles: true,
@@ -186,8 +284,30 @@ Export.image.toDrive({
 Export.image.toDrive({
 		image: mosaic.select(['swir1_median_dry', 'nir_median_dry', 'red_median_dry']),
     description: 'SD-23-Y-C_sentinelMedianDry_v1',
-    folder: 'MAPA_REFERENCIA',
-    region: raw.geometry(),
+    folder: output,
+    region: tile.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: mosaic_prev.select(['swir1_median_dry', 'nir_median_dry', 'red_median_dry']),
+    description: 'SD-23-Y-C_sentinelMedianDryPrev_v1',
+    folder: output,
+    region: tile.geometry(),
+    scale: 10,
+    maxPixels: 1e13,
+    skipEmptyTiles: true,
+    fileFormat: 'GeoTIFF',
+});
+
+Export.image.toDrive({
+		image: mosaic_post.select(['swir1_median_dry', 'nir_median_dry', 'red_median_dry']),
+    description: 'SD-23-Y-C_sentinelMedianDryPost_v1',
+    folder: output,
+    region: tile.geometry(),
     scale: 10,
     maxPixels: 1e13,
     skipEmptyTiles: true,
