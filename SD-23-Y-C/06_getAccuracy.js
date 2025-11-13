50b674bd90e0bf9e1974671c2d1708d968ec51eb
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2025-11-13 16:15:57 -0300

    Update 06_getAccuracy.js

--- SD-23-Y-C/06_getAccuracy.js
+++ SD-23-Y-C/06_getAccuracy.js
@@ -20,6 +20,14 @@ var mapbiomas = ee.Image('users/dh-conciani/gt_mapa_referencia/embeddings/SD-23-
 var mixed = ee.Image('users/dh-conciani/gt_mapa_referencia/embeddings/SD-23-Y-C_classification_MIXED_v1').rename('MIXED')
 var mixedfull = ee.Image('users/dh-conciani/gt_mapa_referencia/embeddings/SD-23-Y-C_classification_MIXEDFULL_v1').rename('MIXEDFULL')
 
+var col10_raw = ee.Image('projects/mapbiomas-workspace/COLECAO_DEV/COLECAO10_DEV/CERRADO/LANDSAT/C10-POST-CLASSIFICATION/CERRADO_C10_gapfill_v11').select('classification_2020')
+  .remap({
+    'from': [3, 4, 5, 6, 49, 11, 12, 29, 50, 13, 15, 19, 39, 20, 40, 62, 41, 46, 47, 35, 48,  9, 21, 23, 24, 75, 30, 25, 33, 31],
+    'to':   [3, 4, 3, 3,  3, 11, 12, 12, 12, 12, 15, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 9,  0,  25, 25, 25, 25, 25, 33, 33]
+  })
+  .rename('COL10_RAW')
+
+
 // read palette
 var vis = {
     'min': 0,
@@ -32,8 +40,8 @@ Map.addLayer(land, vis,'LANDSAT')
 Map.addLayer(aef, vis, 'AEF')
 Map.addLayer(mapbiomas, vis, 'MAPBIOMAS')
 Map.addLayer(mixed, vis, 'MIXED')
-Map.addLayer(mixedfull, vis, 'MIXED')
-
+Map.addLayer(mixedfull, vis, 'MIXEDFULL')
+Map.addLayer(col10_raw, vis, 'COL10_RAW')
 
 ///////////////// compute confusion matrixes
 var region = ref.geometry();
@@ -44,6 +52,7 @@ var samplePoints = land
   .addBands(mapbiomas)
   .addBands(mixed)
   .addBands(mixedfull)
+  .addBands(col10_raw)
   .sample({
     region: region,
     scale: 30,              // adjust to your pixel size
@@ -59,6 +68,7 @@ var acc_aef = samplePoints.errorMatrix('AEF', 'REFERENCE');
 var acc_mapbiomas = samplePoints.errorMatrix('MAPBIOMAS', 'REFERENCE');
 var acc_mixed = samplePoints.errorMatrix('MIXED', 'REFERENCE');
 var acc_mixedfull = samplePoints.errorMatrix('MIXEDFULL', 'REFERENCE');
+var acc_col10_raw = samplePoints.errorMatrix('COL10_RAW', 'REFERENCE');
 
 
 print('LANDSAT', acc_land.accuracy(), acc_land.consumersAccuracy(), acc_land.producersAccuracy())
@@ -66,4 +76,5 @@ print('AEF', acc_aef.accuracy(), acc_aef.consumersAccuracy(), acc_aef.producersA
 print('MAPBIOMAS EMBEDDINGS', acc_mapbiomas.accuracy(), acc_mapbiomas.consumersAccuracy(), acc_mapbiomas.producersAccuracy())
 print('MIXED', acc_mixed.accuracy(), acc_mixed.consumersAccuracy(), acc_mixed.producersAccuracy())
 print('MIXEDFULL', acc_mixedfull.accuracy(), acc_mixedfull.consumersAccuracy(), acc_mixedfull.producersAccuracy())
+print('COL10_RAW', acc_col10_raw.accuracy(), acc_col10_raw.consumersAccuracy(), acc_col10_raw.producersAccuracy())
 
