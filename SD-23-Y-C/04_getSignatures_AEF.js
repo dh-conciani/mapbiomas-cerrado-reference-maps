3445e2a70df2de978e7b3819657c4ca5cf0c8c54
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2025-11-12 22:33:50 -0300

    Update 04_getSignatures_AEF.js

--- SD-23-Y-C/04_getSignatures_AEF.js
+++ SD-23-Y-C/04_getSignatures_AEF.js
@@ -29,7 +29,7 @@ var year = 2020;
 var mosaic = ee.ImageCollection('GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL')
 
 // import sample points
-var samples = ee.FeatureCollection('users/dh-conciani/gt_mapa_referencia/embeddings/SD-23-Y-C_LANDSAT_training_v1');
+var samples = ee.FeatureCollection('users/dh-conciani/gt_mapa_referencia/embeddings/SD-23-Y-C_samplePoints_v1');
 
 // get the sentinel mosaic for the classification year
 var mosaic_i = mosaic
